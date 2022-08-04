45a18bf9517b2214abc0a7f19372074f198393c1
kc <kc713@naver.com>
2022-08-04 10:34:48 +0900

    fix: 게시글 역순으로2

---
+++ backend/src/main/java/com/ssafy/api/request/studyCalendar/StudyCalendarRes.java
@@ -0,0 +1,42 @@
+package com.ssafy.api.request.studyCalendar;
+
+import com.ssafy.db.entity.StudyCalendar;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel("StudyCalendarResponse")
+public class StudyCalendarRes {
+
+	Long groupId;
+	Long user_pk;
+	String title;
+	String content;
+	String writer;
+	String start;
+	String end;
+	String textColor;
+	String backgroundColor;
+	String borderColor;
+	
+	public static StudyCalendarRes of(StudyCalendar sc) {
+		StudyCalendarRes scr = new StudyCalendarRes();
+		scr.setBackgroundColor(sc.getBackgroundColor());
+		scr.setBorderColor(sc.getBorderColor());
+		scr.setContent(sc.getContent());
+		scr.setEnd(sc.getEnd());
+		scr.setGroupId(sc.getStudy_pk());
+		scr.setStart(sc.getStart());
+		scr.setTextColor(sc.getTextColor());
+		scr.setTitle(sc.getTitle());
+		scr.setUser_pk(sc.getUser_pk());
+		scr.setWriter(sc.getWriter());
+		
+		return scr;
+	}
+		
+	
+}
